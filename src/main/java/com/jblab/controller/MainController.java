352e4e07f9171cbb0e51c3545ec3e668f2a40a12
DamirIlyasov <damirilyasov1997@gmail.com>
2017-07-13 11:47:10 +0300

    Index page added, admitad verifying test №2

---
+++ src/main/java/com/jblab/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.jblab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by damir on 13.07.17.
+ */
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String getIndexPage(){
+        return "index";
+    }
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,6 @@
+<head>
+    <meta name="verify-admitad" content="2a89330e58" />
+</head>
+<body>
+<h1>visit /upload!</h1>
+</body>
